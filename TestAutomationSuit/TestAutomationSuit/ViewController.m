5c1f18ea04cdbfe166ae0c0fd8e1014c7a49ffad
Vinay Raj <vinayraj.dunty@myntra.com>
2015-06-25 19:29:57 +0530

    Push schemes to remote

--- TestAutomationSuit/TestAutomationSuit/ViewController.m
+++ TestAutomationSuit/TestAutomationSuit/ViewController.m
@@ -19,6 +19,15 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)viewDidAppear:(BOOL)animated{
+    NSString *target = [[[NSProcessInfo processInfo] environment] objectForKey:@"TARGET"];
+    if( [target isEqualToString:@"PROD"] ){
+        NSLog(@"Log : Prod environment");
+    }else if ([target isEqualToString:@"QA"]){
+        NSLog(@"Log : QA environment");
+    }
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
