7287f0f8e7f50c5792d23cb3ff7442ce3e442e2c
Vinay Raj <vinayraj.dunty@myntra.com>
2015-06-25 19:16:19 +0530

    git ignore added

---
+++ TestAutomationSuit/TestAutomationSuit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestAutomationSuit
+//
+//  Created by Vinay Raj Dunty on 25/06/15.
+//  Copyright (c) 2015 Vinay Raj Dunty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAutomationSuit/TestAutomationSuit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestAutomationSuit
+//
+//  Created by Vinay Raj Dunty on 25/06/15.
+//  Copyright (c) 2015 Vinay Raj Dunty. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
